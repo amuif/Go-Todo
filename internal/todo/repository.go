f83672e05697360ec9a2d77cc3592a7229537173
amuif <amudihayat@gmail.com>
2026-03-01 18:13:25 +0300

    nuking everything and starting from scratch

--- internal/todo/repository.go
+++
@@ -1,9 +0,0 @@
-package todo
-
-type Repository interface {
-	Create(todo *Todo) error
-	GetAll() ([]Todo, error)
-	GetByID(id string) (*Todo, error)
-	Update(todo *Todo) error
-	Delete(id string) error
-}
